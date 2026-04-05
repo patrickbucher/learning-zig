e629710f146b333aacdc63e3c3f4019116d6cf90
Patrick Bucher <patrick.bucher@mailbox.org>
2026-04-05 20:55:06 +0200

    streaming cat

---
+++ examples/ch03/cat_3.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.DebugAllocator(.{}).init;
+    defer _ = gpa.deinit();
+    const ally = gpa.allocator();
+
+    const args = try std.process.argsAlloc(ally);
+    defer std.process.argsFree(ally, args);
+
+    const cwd = std.fs.cwd();
+
+    var stdout_buffer: [1024]u8 = undefined;
+    var stderr_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stderr_writer = std.fs.File.stderr().writer(&stderr_buffer);
+    const stdout = &stdout_writer.interface;
+    const stderr = &stderr_writer.interface;
+
+    for (args[1..]) |filepath| {
+        const file = cwd.openFile(filepath, .{}) catch {
+            stderr.print("could not open: {s}\n", .{filepath}) catch {};
+            stderr.flush() catch {};
+            continue;
+        };
+        defer file.close();
+
+        var file_buffer: [1024]u8 = undefined;
+        var file_reader = file.reader(&file_buffer);
+        const reader = &file_reader.interface;
+    }
+    try stdout.flush();
+}
